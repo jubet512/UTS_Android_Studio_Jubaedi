db56f7c9ee50dadb3345e3a48f328ba1b5fbaa28
jubet512 <jubetnightray@gmail.com>
2019-02-04 20:41:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uts_android_studio_jubaedi/TampilActivity.java
@@ -0,0 +1,13 @@
+package com.example.uts_android_studio_jubaedi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TampilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampil);
+    }
+}
